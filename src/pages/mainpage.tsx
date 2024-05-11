8e85cce643e35100b0c152916c2e2a687e0a45de
Kishore Shankar Abimanyu <kishoreshankarabimanyu@Kishores-MBP.lan>
2024-05-11 01:05:16 -0700

    CI/CD pipeline corrections

--- src/pages/mainpage.tsx
+++ src/pages/mainpage.tsx
@@ -1,11 +1,6 @@
 import React from "react";
-import one from "../resource/one.jpg";
 import Homedesc from "../components/homedesc";
-import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
-import logo2 from '../resource/logos 2/Aerogenesis.png'
-import { styled } from 'styled-components';
-import { Button } from "@aws-amplify/ui-react";
-import TopBar from "../components/topbar";
+
 import '../stylesheets/styles.css'
 const MainPage: React.FC = () => {
   return (
